922c918469dd40f7ba47ea036e0b0b01ead04e39
Matthew James <ma.james0202@gmail.com>
2026-07-07 18:15:17 +0100

    build: added gcc build options

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-MODE="${1:-debug}"
+PRESET="${1:-debug}"
 
-if [[ "$MODE" != "debug" && "$MODE" != "release" ]]; then
-    echo "Usage: $0 [debug|release]"
-    exit 1
-fi
+cmake --preset "$PRESET"
+cmake --build --preset "$PRESET"
 
-cmake --preset "$MODE"
-cmake --build --preset "$MODE"
+ln -sf build/$PRESET/compile_commands.json compile_commands.json
 
-ln -sf build/$MODE/compile_commands.json compile_commands.json
-
-ctest --test-dir "build/$MODE"
+ctest --test-dir "build/$PRESET"
